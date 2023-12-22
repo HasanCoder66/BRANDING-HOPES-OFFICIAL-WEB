8910f25a23871ba2bcc2128c9e7d572d57c72292
HasanCoder66 <hasanashraf874@gmail.com>
2023-12-23 00:10:48 +0500

    proxy set

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -1,34 +1,32 @@
-import {BrowserRouter , Route , Routes} from 'react-router-dom'
-import Home from './components/Home/Home.jsx'
-import Login from './components/Login/Login.jsx'
-import Signup from './components/Signup/Signup.jsx'
-import Blog from './components/Blog/Blog.jsx'
-import Services from './components/Services/Services.jsx'
-import './App.css'
-import Contact from './components/Contact/Contact.jsx'
-import About from './components/About/About.jsx'
-import Package from './components/Package/Package.jsx'
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./components/Home/Home.jsx";
+import Login from "./components/Login/Login.jsx";
+import Signup from "./components/Signup/Signup.jsx";
+import Blog from "./components/Blog/Blog.jsx";
+import Services from "./components/Services/Services.jsx";
+import Contact from "./components/Contact/Contact.jsx";
+import About from "./components/About/About.jsx";
+import Package from "./components/Package/Package.jsx";
 
 function App() {
-
+  
   return (
     <>
-
-<BrowserRouter>
-<Routes>
- <Route path='/' element={<Home />} />
- <Route path='/login' element={<Login />} />
- <Route path='/packages' element={<Package />} />
- <Route path='/signup' element={<Signup />} />
- <Route path='/blog' element={<Blog />} />
- <Route path='/contact' element={<Contact />} />
- <Route path='/services' element={<Services />} />
- <Route path='/about' element={<About />} />
-</Routes>
-</BrowserRouter>
-
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/packages" element={<Package />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
